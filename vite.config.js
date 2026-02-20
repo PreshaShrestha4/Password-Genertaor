82dce2bc1985ad2878b47642affb5f14bcb018f6
PreshaShrestha4 <preshashrestha041@gmail.com>
2026-02-20 13:46:14 +0545

    update config and prepare deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Password-Genertaor/", // match your repo name
   plugins: [react(), tailwindcss()],
 });
